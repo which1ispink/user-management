73f87cfcf2c166bd980a156ad22510aa90be9f3c
Ahmed Hassan <a.hassan.dev@gmail.com>
2017-07-21 22:33:17 +0200

    Remove the REST helper bundles

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,12 @@
+<?php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class UserController
+ */
+class UserController extends Controller
+{
+
+}
